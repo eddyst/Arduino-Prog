f72d3c2bcafd9ab608c786d47da84ac6b95dc409
Eddy <est.git@online.de>
2015-08-11 14:44:09 +0200

    Add Definition to w5100.h

--- libraries/Ethernet/src/utility/w5100.h
+++ libraries/Ethernet/src/utility/w5100.h
@@ -331,7 +331,11 @@ private:
 
 private:
 #if defined(ARDUINO_ARCH_AVR)
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#if defined(CONTROLLINO_MAXI) || defined(CONTROLLINO_MEGA)
+  inline static void initSS()    { DDRJ  |=  _BV(3); };
+  inline static void setSS()     { PORTJ &= ~_BV(3); };
+  inline static void resetSS()   { PORTJ |=  _BV(3); }; 
+#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
   inline static void initSS()    { DDRB  |=  _BV(4); };
   inline static void setSS()     { PORTB &= ~_BV(4); };
   inline static void resetSS()   { PORTB |=  _BV(4); };
